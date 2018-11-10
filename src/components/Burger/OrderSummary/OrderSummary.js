1a2decece1e3ad88b9e218002aa19a69bb28fe7d
ibrahimawadhamid <ibrahim.a.hamid@gmail.com>
2018-11-10 17:38:44 +0200

    Feature Added - Total price to the order summary

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -18,6 +18,7 @@ const orderSummary = (props) => {
             <ul>
                 {ingredientSummary}
             </ul>
+            <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
             <p>Continue to Checkout?</p>
             <Button buttonType="Danger" clicked={props.purchaseCanceled}>Cancel</Button>
             <Button buttonType="Success" clicked={props.purchaseContinued}>Continue</Button>
