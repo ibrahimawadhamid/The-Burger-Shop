a99fd66ae0e53225542f4991d019e5ef995c6fee
ibrahimawadhamid <ibrahim.a.hamid@gmail.com>
2018-11-10 17:35:08 +0200

    Feature Added - Button Component (Continue & Cancel)

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Aux from '../../../hoc/_Aux';
+import Button from '../../UI/Button/Button';
 
 const orderSummary = (props) => {
     const ingredientSummary = Object.keys(props.ingredients)
@@ -18,6 +19,8 @@ const orderSummary = (props) => {
                 {ingredientSummary}
             </ul>
             <p>Continue to Checkout?</p>
+            <Button buttonType="Danger" clicked={props.purchaseCanceled}>Cancel</Button>
+            <Button buttonType="Success" clicked={props.purchaseContinued}>Continue</Button>
         </Aux>
     );
 };
