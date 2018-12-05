5e30c12aa861479284e63584bd4bdbdffb9c97b9
Unknown <ibrahim.a.hamid@gmail.com>
2018-12-05 22:03:58 +0200

    Signup and Signin Added

---
+++ src/store/actions/auth.js
@@ -0,0 +1,46 @@
+import axios from 'axios';
+import * as actionTypes from './actionTypes';
+
+export const authStart = () => {
+    return {
+        type: actionTypes.AUTH_START
+    };
+};
+
+export const authSuccess = (authData) => {
+    return {
+        type: actionTypes.AUTH_SUCCESS,
+        authData: authData
+    };
+};
+
+export const authFail = (error) => {
+    return {
+        type: actionTypes.AUTH_FAIL,
+        error: error
+    };
+};
+
+export const auth = (email, password, isSignup) => {
+    return dispatch => {
+        dispatch(authStart());
+        const authData = {
+            email: email,
+            password: password,
+            returnSecureToken: true
+        };
+        let url = "https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyDUAKbAe6JEaW4A4bMVazw47kOxXXFMmyQ";
+        if(!isSignup) {
+            url = "https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyDUAKbAe6JEaW4A4bMVazw47kOxXXFMmyQ";
+        }
+        axios.post(url, authData)
+            .then(response => {
+                console.log(response);
+                dispatch(authSuccess(response));
+            })
+            .catch(error => {
+                console.log(error);
+                dispatch(authFail(error));
+            });
+    };
+};
