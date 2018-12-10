80c45f48f3188a69aeea6e2a620fc540ac814f1f
ibrahimawadhamid <ibrahim.a.hamid@gmail.com>
2018-12-10 06:39:22 +0200

    Updates

---
+++ precache-manifest.73c41add085f3918f17f5ef137d819bf.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "c4d9c87b871c0a799aba",
+    "url": "/The-Burger-Shop/static/css/main.2c4146cf.chunk.css"
+  },
+  {
+    "revision": "c4d9c87b871c0a799aba",
+    "url": "/The-Burger-Shop/static/js/main.c4d9c87b.chunk.js"
+  },
+  {
+    "revision": "828619e395d8da277b4b",
+    "url": "/The-Burger-Shop/static/css/1.c4f2b0fa.chunk.css"
+  },
+  {
+    "revision": "828619e395d8da277b4b",
+    "url": "/The-Burger-Shop/static/js/1.828619e3.chunk.js"
+  },
+  {
+    "revision": "9ab249a914caa339ac2c",
+    "url": "/The-Burger-Shop/static/css/2.0a13ad43.chunk.css"
+  },
+  {
+    "revision": "9ab249a914caa339ac2c",
+    "url": "/The-Burger-Shop/static/js/2.9ab249a9.chunk.js"
+  },
+  {
+    "revision": "c464adebb1c47e1a8968",
+    "url": "/The-Burger-Shop/static/css/3.996faef0.chunk.css"
+  },
+  {
+    "revision": "c464adebb1c47e1a8968",
+    "url": "/The-Burger-Shop/static/js/3.c464adeb.chunk.js"
+  },
+  {
+    "revision": "5e05adc1089d40784e9e",
+    "url": "/The-Burger-Shop/static/js/4.5e05adc1.chunk.js"
+  },
+  {
+    "revision": "a496bb99e3fcbfb6f7a6",
+    "url": "/The-Burger-Shop/static/js/runtime~main.a496bb99.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/The-Burger-Shop/static/media/burger-logo.b8503d26.png"
+  },
+  {
+    "revision": "aa6aad7dc80ec3739ce1f55d0772918c",
+    "url": "/The-Burger-Shop/index.html"
+  }
+];
